9ec6536d0da972d878545486b6066ba725670a4e
Traun Leyden <traun.leyden@gmail.com>
2025-09-14 16:55:59 +0200

    remove tabbed view

---
+++ app/index.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet } from 'react-native';
+import { ThemedText } from '@/components/themed-text';
+import { ThemedView } from '@/components/themed-view';
+
+export default function HomeScreen() {
+  return (
+    <ThemedView style={styles.container}>
+      <ThemedText style={styles.hello}>hello</ThemedText>
+    </ThemedView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  hello: {
+    fontSize: 24,
+  },
+});
